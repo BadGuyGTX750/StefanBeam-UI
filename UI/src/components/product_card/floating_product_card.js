0adb725b0643ab6ff775ea304012ead14c54eec9
BadGuyGTX750 <stefanstefan211@gmail.com>
2023-04-15 16:05:10 +0300

    Floating product page has better looks and functionality

--- UI/src/components/product_card/floating_product_card.js
+++ UI/src/components/product_card/floating_product_card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./floating_product_card.css"
 import Selector3 from "../selector/Selector3";
 import Selector4 from "../selector/Selector4";
@@ -6,14 +6,29 @@ import AddToCartButton from "../button/AddToCartButton";
 
 export default function FloatingProductCard(props) {
 
+  const [priceActual, setPriceActual] = useState(null)
   const closeMethod = props.closeMethod
   const photoSrc = props.photoSrc
   const title = props.title
   const weight_price = props.weight_price
   const weight = weight_price.map((item) => {return item.weight})
+  if (priceActual == null)
+    setPriceActual(weight_price[0].price)
   const flavor_quantity = props.flavor_quantity
   const flavor = flavor_quantity.map((item) => {return item.flavor})
-  console.log(flavor)
+
+  const HandlePackagingChange = (pkg) => {
+    console.log(pkg)
+    // eslint-disable-next-line 
+    weight_price.map((item) => {
+      if (item.weight === pkg)
+        setPriceActual(item.price)
+    })
+  }
+
+  const HandleFlavorChange = (flv) => {
+    console.log(flv)
+  }
   
   return(
     <div className="floating-product-card-background">
@@ -31,19 +46,19 @@ export default function FloatingProductCard(props) {
           <h3> { title + ' - StefanBeam' } </h3>
         </div>
 
-        <div className="floating-product-card-price-packaging">
-          <div className="floating-product-card-price">
-            <p>Packaging *</p>
-            <Selector3 ddItems={weight}/>
-          </div>
-          <div className="floating-product-card-packaging">
-            <p>Flavor/Size *</p>
-            <Selector4 ddItems={flavor}/>
-          </div>
+        <div className="floating-product-card-price">
+          { "$" + priceActual }
         </div>
 
-        <div className="floating-product-card-flavor">
-          
+        <div className="floating-product-card-packaging-flavor">
+          <div className="floating-product-card-packaging">
+            <p>Packaging *</p>
+            <Selector3 method={HandlePackagingChange} ddItems={weight}/>
+          </div>
+          <div className="floating-product-card-flavor">
+            <p>Flavor/Size *</p>
+            <Selector4 method={HandleFlavorChange} ddItems={flavor}/>
+          </div>
         </div>
 
         <div className="floating-product-card-add-to-cart">
